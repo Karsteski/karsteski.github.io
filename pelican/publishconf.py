62f2a9af3d68232e9784b87591e2dcac40d55cc8
Kareem Skinner <kareem@karsteski.com>
2021-08-06 18:25:00 -0400

    test

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -12,20 +12,3 @@ except ImportError:
 SITEURL = "https://www.karsteski.com"
 
 RELATIVE_URLS = False
-
-USE_LESS = False
-
-GOOGLE_ANALYTICS = "UA-55543164-6"
-
-GOOGLE_ADSENSE = {
-    "ca_id": "ca-pub-6625957038449899",
-    "page_level_ads": True,
-    "ads": {
-        "aside": "8752710348",
-        "main_menu": "",
-        "index_top": "",
-        "index_bottom": "1124188687",
-        "article_top": "",
-        "article_bottom": "4843941849",
-    },
-}
