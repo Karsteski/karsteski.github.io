5e31204519c60df79e83f304c31dc2dd9f14e1ae
Kareem Skinner <kareem@karsteski.com>
2021-08-06 17:01:58 -0400

    example of themes not working

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -1,34 +1,79 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+from datetime import datetime
 
-AUTHOR = 'Kareem Skinner'
-SITENAME = 'Karsteski Test Page'
-SITEURL = ''
+AUTHOR = "Alexandre Vicenzi"
+SITEURL = "http://localhost:8000"
+SITENAME = "Flex"
+SITETITLE = "Flex"
+SITESUBTITLE = "The minimalist Pelican theme"
+SITEDESCRIPTION = "Flex - The minimalist Pelican theme."
+# SITELOGO = ''
+# FAVICON = '/images/favicon.ico'
+BROWSER_COLOR = "#333333"
+PYGMENTS_STYLE = "monokai"
 
-PATH = 'content'
+ROBOTS = "index, follow"
 
-TIMEZONE = 'Canada/Eastern'
+THEME = "/home/karsteski/Repos/karsteski.github.io/pelican/Flex/"
+PATH = "content"
+OUTPUT_PATH = "output"
+TIMEZONE = "America/New_York"
 
-DEFAULT_LANG = 'en'
+# PLUGIN_PATHS = ['pelican-plugins']
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+# PLUGINS = ['i18n_subsites']
+
+# JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+I18N_TEMPLATES_LANG = "en"
+DEFAULT_LANG = "en"
+OG_LOCALE = "en_US"
+LOCALE = "en_US"
+
+DATE_FORMATS = {
+    "en": "%B %d, %Y",
+}
+
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("github", "https://github.com/alexandrevicenzi/Flex"),
+    ("rss", "/blog/feeds/all.atom.xml"),
+)
 
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa",
+}
+
+COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+DISQUS_SITENAME = "flex-pelican"
+ADD_THIS_ID = "ra-55adbb025d4f7e55"
+
+STATIC_PATHS = ["images", "extra/ads.txt", "extra/CNAME"]
+
+EXTRA_PATH_METADATA = {
+    "extra/ads.txt": {"path": "ads.txt"},
+    "extra/CNAME": {"path": "CNAME"},
+}
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
